b6396e027ecce1e77d77b486e1e7687d76e89f8b
IlamSingleBrainCell <ilamvazhuthi.pro@gmail.com>
2025-07-08 00:28:22 +0530

    Update PatternVisualization.js

--- src/components/PatternVisualization.js
+++ src/components/PatternVisualization.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { drawPattern } from '../utils/patternDrawing';
 
 // Pattern Visualization Component
